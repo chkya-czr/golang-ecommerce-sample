0939046c6c5d02bb094cf9482fb2e7a199ab8d99
Aayush Gogia <gogia.a.dev@gmail.com>
2024-07-11 00:38:50 +0530

    add new config files

--- product_service/config/config.go
+++ product_service/config/config.go
@@ -1,56 +1,24 @@
 package config
 
 import (
-	"github.com/rs/zerolog/log"
-	"github.com/spf13/viper"
+	"log"
+
+	"github.com/joho/godotenv"
 )
 
-type AppConfig struct {
-	Env  string `mapstructure:"APP_ENV"`
-	Name string `mapstructure:"APP_NAME"`
-}
-
-type DatabaseConfig struct {
-	Debug    int    `mapstructure:"DB_DEBUG"`
-	Host     string `mapstructure:"DB_HOST"`
-	Name     string `mapstructure:"DB_NAME"`
-	Port     string `mapstructure:"DB_PORT"`
-	Password string `mapstructure:"DB_PASSWORD"`
-	User     string `mapstructure:"DB_USER"`
-}
-
-type ServerConfig struct {
-	APIDocsPort int `mapstructure:"HTTP_PORT_API_DOCS"`
-	Port        int `mapstructure:"HTTP_PORT"`
-}
-
 type Config struct {
-	App      AppConfig      `mapstructure:",squash"`
-	Database DatabaseConfig `mapstructure:",squash"`
-	Server   ServerConfig   `mapstructure:",squash"`
+	Api
+	Cors
 }
 
-var LoadedConfig Config
-
-func LoadConfig(path string) (config Config) {
-	viper.AddConfigPath(path)
-	viper.SetConfigFile(".env")
-
-	viper.AutomaticEnv()
-
-	err := viper.ReadInConfig()
+func New() *Config {
+	err := godotenv.Load()
 	if err != nil {
-		log.Panic().Err(err).Msg("[APP] cannot initialize configuration")
+		log.Println(err)
 	}
 
-	err = viper.Unmarshal(&config)
-	if err != nil {
-		log.Panic().Err(err).Msg("[APP] cannot initialize configuration")
+	return &Config{
+		Api:  API(),
+		Cors: NewCors(),
 	}
-
-	log.Info().Msg("[APP] config was loaded successfully")
-
-	LoadedConfig = config
-
-	return config
 }
