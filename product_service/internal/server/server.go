c07c7cc2a86ebbaac2ac8773b0c7305f615d9d79
Aayush Gogia <gogia.a.dev@gmail.com>
2024-07-11 00:38:07 +0530

    server refactor

---
+++ product_service/internal/server/server.go
@@ -0,0 +1,140 @@
+package server
+
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"product_service/config"
+	validate "product_service/external"
+	"product_service/internal/middleware"
+	"product_service/logger"
+	"syscall"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	chiMiddleware "github.com/go-chi/chi/v5/middleware"
+	"github.com/go-playground/validator/v10"
+	"github.com/rs/cors"
+)
+
+type Server struct {
+	Version string
+	cfg     *config.Config
+
+	validator *validator.Validate
+	cors      *cors.Cors
+	router    *chi.Mux
+
+	httpServer *http.Server
+}
+
+func (s *Server) Config() *config.Config {
+	return s.cfg
+}
+
+func New() *Server {
+	return &Server{
+		cfg:    config.New(),
+		router: chi.NewRouter(),
+	}
+
+}
+
+func (s *Server) initLog() {
+	slog.SetDefault(slog.New(logger.NewTraceHandler(
+		os.Stdout,
+		&slog.HandlerOptions{},
+	)))
+}
+
+func (s *Server) setCors() {
+	s.cors = cors.New(
+		cors.Options{
+			AllowedOrigins: s.cfg.Cors.AllowedOrigins,
+			AllowedMethods: []string{
+				http.MethodHead,
+				http.MethodGet,
+				http.MethodPost,
+				http.MethodPut,
+				http.MethodPatch,
+				http.MethodDelete,
+			},
+			AllowedHeaders:   []string{"*"},
+			AllowCredentials: true,
+		})
+}
+
+func (s *Server) newValidator() {
+	s.validator = validate.New()
+}
+
+func (s *Server) Init() {
+	s.initLog()
+	s.setCors()
+	s.newValidator()
+	s.newRouter()
+	s.setGlobalMiddleware()
+}
+
+func (s *Server) newRouter() {
+	s.router = chi.NewRouter()
+}
+
+func (s *Server) setGlobalMiddleware() {
+	s.router.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+		_, _ = w.Write([]byte(`{"message": "endpoint not found"}`))
+	})
+	s.router.Use(s.cors.Handler)
+	s.router.Use(middleware.Json)
+	s.router.Use(middleware.Audit)
+	if s.cfg.Api.RequestLog {
+		s.router.Use(chiMiddleware.Logger)
+	}
+	s.router.Use(chiMiddleware.Recoverer)
+}
+
+func (s *Server) Run() {
+	s.httpServer = &http.Server{
+		Addr:              s.cfg.Api.Host + ":" + s.cfg.Api.Port,
+		Handler:           s.router,
+		ReadHeaderTimeout: s.cfg.Api.ReadHeaderTimeout,
+	}
+
+	go func() {
+		start(s)
+	}()
+
+	_ = gracefulShutdown(context.Background(), s)
+}
+
+func start(s *Server) {
+	log.Printf("Serving at %s:%s\n", s.cfg.Api.Host, s.cfg.Api.Port)
+	err := s.httpServer.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func gracefulShutdown(ctx context.Context, s *Server) error {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+
+	log.Println("Shutting down...")
+
+	ctx, shutdown := context.WithTimeout(ctx, s.Config().Api.GracefulTimeout*time.Second)
+	defer shutdown()
+
+	err := s.httpServer.Shutdown(ctx)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return nil
+}
